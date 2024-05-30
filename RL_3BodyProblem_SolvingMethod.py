f0a5d1b906a0adc99101dd4956a82d3e94070237
Joao Luis Ritton Costa <jlrittoncosta@usp.br>
2024-05-30 17:59:37 -0300

    Setup solution structure

---
+++ RL_3BodyProblem_SolvingMethod.py
@@ -0,0 +1,21 @@
+# Imports
+import numpy as np
+import math
+
+# Constants
+G = 6.67430e-11
+
+# Functions
+def GaussianElimination(A:np.array, b:np.array) -> np.array:
+    pass
+
+# Classes
+class ThreeBodyProblem:
+    def __init__(self, m:tuple, X1:tuple, X2:tuple, X3:tuple, T, t0=0, n=1000) -> None:
+        pass
+    def _setDerivatives(self) -> function:
+        pass
+    def _RungeKutta44(self) -> np.array:
+        pass
+    def _SplineInterpolation(self) -> list[function]:
+        pass
