7c651da3fcc2e6f270137281193cf4570e6c4cb8
Joao Luis Ritton Costa <jlrittoncosta@usp.br>
2024-06-07 22:36:30 -0300

    RungeKutta44 bugs fiz

--- RL_3BodyProblem_SolvingMethod.py
+++ RL_3BodyProblem_SolvingMethod.py
@@ -3,12 +3,13 @@ import numpy as np
 import math
 
 # Constants
-G = 6.67430e-11
+#G = 6.67430e-11
 
 # Functions
 def GaussianElimination(A:np.array, b:np.array) -> np.array:
-    n = len(A)
-    for i in range(n):
+    n = len(b)
+    print(b.shape)
+    for i in range(n-1):
         # Partial pivoting
         max_row = i
         for j in range(i+1, n):
@@ -32,19 +33,15 @@ def GaussianElimination(A:np.array, b:np.array) -> np.array:
 
 # Classes
 class ThreeBodyProblem:
-    def __init__(self, m:tuple, X11:tuple, X12:tuple, X13:tuple, X21:tuple, X22:tuple, X23:tuple, T, t0=0, n=1000) -> None:
+    def __init__(self, m:tuple, X: np.array, T, t0=0, n=1000, G=6.67430e-11) -> None:
         # Set system parameters
         self.m = m
-        self.X11 = X11
-        self.X12 = X12
-        self.X13 = X13
-        self.X21 = X21
-        self.X22 = X22
-        self.X23 = X23
+        self.X = X
         self.T = T
         self.t0 = t0
         self.n = n
         self.delta = (T-t0)/n
+        self.G = G
 
         # Solve the system
         # Call Runge-Kutta 4-4 method for solving the system
@@ -55,21 +52,21 @@ class ThreeBodyProblem:
     def f(self, X, t):
         m1, m2, m3 = self.m
         x_dot = np.zeros(len(X))
-        x_dot[0:3] = X[10:13]
-        x_dot[3:6] = X[13:16]
-        x_dot[6:9] = X[16:19]
-        x_dot[10:13] = -G*m2*(X[0:3]-X[3:6])/np.linalg.norm(X[0:3]-X[3:6])**3 - G*m3*(X[0:3]-X[6:9])/np.linalg.norm(X[0:3]-X[6:9])**3
-        x_dot[13:16] = -G*m1*(X[3:6]-X[0:3])/np.linalg.norm(X[3:6]-X[0:3])**3 - G*m3*(X[3:6]-X[6:9])/np.linalg.norm(X[3:6]-X[6:9])**3
-        x_dot[16:19] = -G*m1*(X[6:9]-X[0:3])/np.linalg.norm(X[6:9]-X[0:3])**3 - G*m2*(X[6:9]-X[3:6])/np.linalg.norm(X[6:9]-X[3:6])**3
+        x_dot[0:3] = X[9:12]
+        x_dot[3:6] = X[12:15]
+        x_dot[6:9] = X[15:18]
+        x_dot[9:12] = -self.G*m2*(X[0:3]-X[3:6])/np.linalg.norm(X[0:3]-X[3:6])**3 - self.G*m3*(X[0:3]-X[6:9])/np.linalg.norm(X[0:3]-X[6:9])**3
+        x_dot[12:15] = -self.G*m1*(X[3:6]-X[0:3])/np.linalg.norm(X[3:6]-X[0:3])**3 - self.G*m3*(X[3:6]-X[6:9])/np.linalg.norm(X[3:6]-X[6:9])**3
+        x_dot[15:18] = -self.G*m1*(X[6:9]-X[0:3])/np.linalg.norm(X[6:9]-X[0:3])**3 - self.G*m2*(X[6:9]-X[3:6])/np.linalg.norm(X[6:9]-X[3:6])**3
         return x_dot
 
     def _RungeKutta44(self) -> np.array:
-        self.Xs = np.array([])
         self.t = np.array([])
-        x = np.array([self.X11, self.X12, self.X13, self.X21, self.X22, self.X2])
+        x = self.X
         t = self.t0
+        self.Xs = []
         for i in range(self.n):
-            self.Xs = np.append(self.Xs, x)
+            self.Xs.append(x)
             self.t = np.append(self.t, t)
             k1 = self.f(x, self.t)
             k2 = self.f(x + self.delta/2*k1, self.t + self.delta/2)
@@ -85,17 +82,17 @@ class ThreeBodyProblem:
             t_diff2 = self.t[i+1] - self.t[i]
             zeros = np.zeros(len(self.Xs[i]))
             base = np.ones(len(self.Xs[i]))
-            A = np.array([
-                [base,zeros,zeros,zeros,zeros,zeros,zeros,zeros],
-                [base, t_diff1*base, (t_diff1**2)*base, (t_diff1**3)*base, zeros, zeros, zeros, zeros],
-                [zeros,zeros,zeros,zeros,base,zeros,zeros,zeros],
-                [zeros,zeros,zeros,zeros,base, t_diff2*base, (t_diff2**2)*base, (t_diff2**3)*base],
-                [zeros,base,(2*t_diff1)*base,(3*t_diff1**2)*base,zeros,-base,zeros,zeros],
-                [zeros,zeros,2*base,(6*t_diff1)*base,zeros,zeros,-2*base,zeros],
-                [zeros,base,zeros,zeros,zeros,zeros,zeros,zeros],
-                [zeros,zeros,zeros,zeros,zeros,base,(2*t_diff2)*base,(3*t_diff2**2)*base]
-            ])
-            b = np.array([self.Xs[i-1], self.Xs[i], self.Xs[i], self.Xs[i+1], zeros, zeros, self.f(self.Xs[i-1],self.t[i-1]), self.f(self.Xs[i+1],self.t[i+1])])
+            A = np.vstack((
+                np.array([base,zeros,zeros,zeros,zeros,zeros,zeros,zeros]),
+                np.array([base, t_diff1*base, (t_diff1**2)*base, (t_diff1**3)*base, zeros, zeros, zeros, zeros]),
+                np.array([zeros,zeros,zeros,zeros,base,zeros,zeros,zeros]),
+                np.array([zeros,zeros,zeros,zeros,base, t_diff2*base, (t_diff2**2)*base, (t_diff2**3)*base]),
+                np.array([zeros,base,(2*t_diff1)*base,(3*t_diff1**2)*base,zeros,-base,zeros,zeros]),
+                np.array([zeros,zeros,2*base,(6*t_diff1)*base,zeros,zeros,-2*base,zeros]),
+                np.array([zeros,base,zeros,zeros,zeros,zeros,zeros,zeros]),
+                np.array([zeros,zeros,zeros,zeros,zeros,base,(2*t_diff2)*base,(3*t_diff2**2)*base])
+            ))
+            b = np.hstack((self.Xs[i-1], self.Xs[i], self.Xs[i], self.Xs[i+1], zeros, zeros, self.f(self.Xs[i-1],self.t[i-1]), self.f(self.Xs[i+1],self.t[i+1])))
             self.S_params.append(GaussianElimination(A, b)[0:4])
     
     def S(self, t) -> np.array:
